00209304afa8bbdaf44433a2a859cbafd4df134c
DurianDan <huyk25sinh@gmail.com>
2021-07-31 20:09:30 +0700

    add .gitignore

--- all_functions.py
+++ all_functions.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from googletrans import Translator
 from gtts import gTTS
 from os import listdir
 
 options = Options()
 options.page_load_strategy = 'none'
 driver = webdriver.Chrome(options=options)
+
+translator = Translator()
 
 class mandarin_json():
     def __init__(self,mandarin,directory):
@@ -33,17 +36,31 @@ class mandarin_json():
             """//*[@id="hwWord"]""").send_keys(
                 self.word,Keys.ENTER)
         print("* sent words to search-translate bar")
-        #2.get all the meaning and pinyin metadata
+        #2.get all the meaning,example and pinyin metadata
         try:
+            #2.1 the meaning found on website
             self.meanings.update({self.word:driver.find_element_by_xpath(
                 '''/html/body/div/main/div/div[3]/table[1]/tbody/tr[1]/td[2]''').text})
             self.pinyins.update({self.pinyins:driver.find_element_by_xpath(
                 '''/html/body/div/main/div/div[3]/div[1]/div/span'''
             ).text})
-            print("* get meaning and pinyin of the word (first_try)")
+            print("* got meaning and pinyin of the word (first_try)")
+            (driver.find_element_by_xpath(
+                '''/html/body/div/main/div/div[2]/ul/li[3]/a''')).click()
+            example_box = (driver.find_element_by_xpath(
+                '''/html/body/div[1]/main/div/div[3]/table[2]/tbody/tr/td''').text).split('\n')
+            for i in range(0,len(example_box),2):
+                self.Examples.update({example_box[i]:example_box[i+1]})
+            print("* got the examples of the whole word")
         except NoSuchElementException:
+            #2.2 had to devide into multiple letters to find meaning
+            print("* coudn't find examples on yellowbridge.com")
             print("""* Couldn't find meaning of whole word,
                         finding the meaning of component words...""")
+            self.meanings.update({
+                self.word:translator(self.word,dest="en",src="zh").text
+            })
+            print("* got the google translated meaning")
             for i in range(len(self.word)):
                 print("** getting list of component words...")
                 try:
@@ -84,4 +101,5 @@ class mandarin_json():
         print("successfully get Hán Việt Meanings")
         driver.quit()     
     def get_Examples(self):
+        '''get examples on chinesepod.com'''
         pass
